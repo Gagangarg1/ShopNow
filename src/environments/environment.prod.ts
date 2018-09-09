aa600bd6c9b2b40f4c48e45b5b668211b434e7ff
Gagan_garg <garg.gagan@outlook.com>
2018-09-09 22:57:40 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAHoieI8yIOXilwE127tNcaj7i6oA-MhqY",
+    authDomain: "shopnow-2bb8f.firebaseapp.com",
+    databaseURL: "https://shopnow-2bb8f.firebaseio.com",
+    projectId: "shopnow-2bb8f",
+    storageBucket: "",
+    messagingSenderId: "336121571242"
+  }
+};
